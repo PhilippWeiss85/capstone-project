468538fd518962d2511da2785f986e1e793bd5e9
Philipp Weiss <p.weiss85@gmx.de>
2022-10-17 14:51:09 +0200

    add custom fonts

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -5,6 +5,17 @@ const GlobalStyle = createGlobalStyle`
           --text-primary: #0c2c4d;
           --background-primary: #edf0f5;
       }
+
+      /* noto-sans-regular - latin */
+        @font-face 
+      {
+        font-family: 'Noto Sans';
+        font-style: normal;
+        font-weight: 400;
+        src: local(''),
+            url('/fonts/noto-sans-v27-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+            url('/fonts/noto-sans-v27-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      }
   
       * {
           box-sizing: border-box;
