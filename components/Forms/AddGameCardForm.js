c6ef03cc9a2d62a41b19a0b6d06508c1a8259cf6
Philipp Weiss <p.weiss85@gmx.de>
2022-10-18 12:56:37 +0200

    fix StyledCardContainer from li to ul in GameCard.js

--- components/Forms/AddGameCardForm.js
+++ components/Forms/AddGameCardForm.js
@@ -1,15 +1,15 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export default function AddGameCardForm() {
-  return (
-    <form>
-      <fieldset>
-        <legend>Match Type:</legend>
-        <label forhtml="match">Match</label>
-        <input type="radio" name="radio-button" id="match" value="match"></input>
-        <label forhtml="training">Training</label>
-        <input type="radio" name="radio-button" id="training" value="training"></input>
-      </fieldset>
-    </form>
-  );
-}
+// export default function AddGameCardForm() {
+//   return (
+//     <form>
+//       <fieldset>
+//         <legend>Match Type:</legend>
+//         <label forhtml="match">Match</label>
+//         <input type="radio" name="radio-button" id="match" value="match"></input>
+//         <label forhtml="training">Training</label>
+//         <input type="radio" name="radio-button" id="training" value="training"></input>
+//       </fieldset>
+//     </form>
+//   );
+// }
