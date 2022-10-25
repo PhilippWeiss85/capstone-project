943cbd2e25b0fb35032cec922a02c073e6e01277
Philipp Weiss <p.weiss85@gmx.de>
2022-10-25 14:34:42 +0200

    change alert message

--- components/CardDetail/CardDetail.js
+++ components/CardDetail/CardDetail.js
@@ -26,7 +26,7 @@ export default function CardDetails({ updateCardDetail, id, showMoreDetails }) {
       );
       showMoreDetails();
     } else {
-      alert("Please enter both score for each set");
+      alert("Please enter both set scores");
     }
   }
 
