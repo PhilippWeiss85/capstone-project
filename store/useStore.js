f4bc344dd257162c7d86d176e4aa261651744a0b
Philipp Weiss <p.weiss85@gmx.de>
2022-10-26 10:30:26 +0200

    install zustand, initial return of global state as object

---
+++ store/useStore.js
@@ -0,0 +1,86 @@
+import create from "zustand/react";
+
+const useStore = create((set) => {
+  return {
+    games: [
+      {
+        id: nanoid(),
+        type: "Match",
+        name: "Max",
+        date: "2022-10-01",
+        time: "19:00",
+        place: "Rothof",
+        court: "Carpet",
+        results: {
+          gameresult: undefined,
+          set: [
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+          ],
+        },
+      },
+      {
+        id: nanoid(),
+        type: "Training",
+        name: "Sonja",
+        date: "2022-15-01",
+        time: "20:00",
+        place: "Fidelopark",
+        court: "Sand",
+        results: {
+          gameresult: undefined,
+          set: [
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+          ],
+        },
+      },
+      {
+        id: nanoid(),
+        type: "Match",
+        name: "Steffi",
+        date: "2022-20-01",
+        time: "10:00",
+        place: "Sportscheck",
+        court: "Gras",
+        results: {
+          gameresult: undefined,
+          set: [
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+            {
+              Player1: "",
+              Player2: "",
+            },
+          ],
+        },
+      },
+    ],
+  };
+});
