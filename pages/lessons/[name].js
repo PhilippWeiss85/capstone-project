997eb2f9ff089e400da8f36d9ea53558d0fab9bd
Philipp Weiss <p.weiss85@gmx.de>
2022-10-27 17:03:20 +0200

    add getStaticPath & getStaticProps

--- pages/lessons/[name].js
+++ pages/lessons/[name].js
@@ -1,13 +1,66 @@
-export default function Lessons() {
+import { getLessonByName, getAllLessons } from "../../lib/db";
+import Image from "next/image";
+
+export function getStaticPaths() {
+  const lessons = getAllLessons();
+  const names = lessons.map((lesson) => lesson.name);
+
+  return {
+    paths: names.map((name) => ({ params: { name: name } })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps(context) {
+  const { name } = context.params;
+  const lessons = getLessonByName(name);
+
+  return {
+    props: {
+      id: lessons.id,
+      name: lessons.name,
+      description: lessons.description,
+      image: lessons.image,
+      alt: lessons.alt,
+      step1: lessons.step1,
+      step2: lessons.step2,
+      step3: lessons.step3,
+      step4: lessons.step4,
+    },
+  };
+}
+
+export default function SingleLesson({
+  name,
+  description,
+  image,
+  alt,
+  step1,
+  step2,
+  step3,
+  step4,
+}) {
   return (
     <>
-      <h1>Lesson Name</h1>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <h1>{name}</h1>
+      <section>
+        <Image
+          src={image}
+          alt={alt}
+          layout="responsive"
+          objectFit="contain"
+          width={1024}
+          height={512}
+        />
+        <article>
+          <ul>
+            <li>{step1}</li>
+            <li>{step2}</li>
+            <li>{step3}</li>
+            <li>{step4}</li>
+          </ul>
+        </article>
+      </section>
     </>
   );
 }
--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,143 +1,38 @@
-import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
+import { getAllLessons } from "../../lib/db";
 
-export default function LessonsList() {
+export function getStaticProps() {
+  const lessons = getAllLessons();
+
+  return {
+    props: { lessons: lessons },
+  };
+}
+
+export default function LessonsList({ lessons }) {
   return (
     <>
       <h1>Your Training Progress</h1>
-      <MainWrapper>
-        <ForehandContainer>
-          <LessonHeadline>Forehand</LessonHeadline>
-          <Link href={`/lessons/`}>
-            <Image
-              src="/assets/forehand_female.jpg"
-              alt="forehand lesson"
-              layout="responsive"
-              objectFit="contain"
-              width={1024}
-              height={512}
-            />
-          </Link>
-        </ForehandContainer>
-        <BackhandContainer>
-          <LessonHeadline>Backhand</LessonHeadline>
-          <Link href={`/lessons/`}>
-            <Image
-              src="/assets/backhand_female.jpg"
-              alt="backhand lesson"
-              layout="responsive"
-              objectFit="contain"
-              width={1024}
-              height={512}
-            />
-          </Link>
-        </BackhandContainer>
-        <SliceContainer>
-          <LessonHeadline>Slice</LessonHeadline>
-          <Link href={`/lessons/`}>
-            <Image
-              src="/assets/backhand_slice_male.jpg"
-              alt="slice lesson"
-              layout="responsive"
-              objectFit="contain"
-              width={1024}
-              height={512}
-            />
-          </Link>
-        </SliceContainer>
-        <VolleyContainer>
-          <LessonHeadline>Serve</LessonHeadline>
-          <Link href={`/lessons/`}>
-            <Image
-              src="/assets/serve_male.jpg"
-              alt="serve lesson"
-              layout="responsive"
-              objectFit="contain"
-              width={1024}
-              height={512}
-            />
-          </Link>
-        </VolleyContainer>
-        <ServeContainer>
-          <LessonHeadline>Volley</LessonHeadline>
-          <Link href={`/lessons/`}>
-            <Image
-              src="/assets/volley_male.jpg"
-              alt="volley lesson"
-              layout="responsive"
-              objectFit="contain"
-              width={1024}
-              height={512}
-            />
-          </Link>
-        </ServeContainer>
-      </MainWrapper>
+      <>
+        {lessons.map((lesson) => (
+          <section key={lesson.id}>
+            <h2>{lesson.name}</h2>
+            <Link href={`/lessons/${lesson.name}`}>
+              <a>
+                <Image
+                  src={lesson.image}
+                  alt={lesson.alt}
+                  layout="responsive"
+                  objectFit="contain"
+                  width={1024}
+                  height={512}
+                />
+              </a>
+            </Link>
+          </section>
+        ))}
+      </>
     </>
   );
 }
-
-const MainWrapper = styled.main`
-  text-align: center;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 2em;
-`;
-
-const LessonHeadline = styled.h2`
-  z-index: 100;
-  position: relative;
-`;
-
-const ForehandContainer = styled.section`
-  grid-area: forehand;
-  position: relative;
-  overflow: hidden;
-  width: 40%;
-  max-width: 512px;
-  height: 25vh;
-  padding: 0;
-  border-radius: 5px;
-`;
-
-const BackhandContainer = styled.section`
-  grid-area: backhand;
-  position: relative;
-  overflow: hidden;
-  width: 40%;
-  max-width: 512px;
-  height: 25vh;
-  padding: 0;
-  border-radius: 5px;
-`;
-const SliceContainer = styled.section`
-  grid-area: slice;
-  position: relative;
-  overflow: hidden;
-  width: 40%;
-  max-width: 512px;
-  height: 25vh;
-  padding: 0;
-  border-radius: 5px;
-`;
-const VolleyContainer = styled.section`
-  grid-area: volley;
-  position: relative;
-  overflow: hidden;
-  width: 40%;
-  max-width: 512px;
-  height: 25vh;
-  padding: 0;
-  border-radius: 5px;
-`;
-const ServeContainer = styled.section`
-  grid-area: serve;
-  position: relative;
-  overflow: hidden;
-  width: 40%;
-  max-width: 512px;
-  height: 25vh;
-  padding: 0;
-  border-radius: 5px;
-`;
