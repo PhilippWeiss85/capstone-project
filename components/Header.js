2a0b37cd576b57986ff482bbee52cf6ea0d76ea3
Philipp Weiss <p.weiss85@gmx.de>
2022-11-02 17:05:04 +0100

    small updates

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { useRouter } from "next/router";
-import { BsList } from "react-icons/bs";
+import { BsChevronDoubleLeft } from "react-icons/bs";
 
 export default function Header() {
   const router = useRouter();
 
   return (
     <HeadWrapper>
+      <PreviousPageIcon onClick={() => router.back()}>
+        <BsChevronDoubleLeft />
+      </PreviousPageIcon>
       {router.pathname === "/gamelist" ? (
         <h1>Gamelist Headline</h1>
       ) : router.pathname === "/lessons" ? (
@@ -16,9 +19,7 @@ export default function Header() {
       ) : (
         router.pathname === "/form" && <h1>Create a new card</h1>
       )}
-      <StyledNav>
-        <BsList />
-      </StyledNav>
+      <StyledNav></StyledNav>
     </HeadWrapper>
   );
 }
@@ -26,14 +27,27 @@ export default function Header() {
 const HeadWrapper = styled.main`
   background-color: var(--background-navigation);
   margin-bottom: 2em;
-
+  padding: 0 2em;
   border-bottom-left-radius: 25px;
   border-bottom-right-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const StyledNav = styled.nav`
-  position: absolute;
   top: 0.7em;
   right: 0.2em;
   font-size: 2em;
+`;
+
+const PreviousPageIcon = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  font-size: 1.5em;
 `;
